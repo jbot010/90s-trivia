e861278f4e0f90beb182b8967fc9c7d12ed13157
jfranco <julian.franco@icloud.com>
2023-03-31 12:09:44 -0400

    built html template, test prompts and answers in data files

---
+++ data/answers.js
@@ -0,0 +1,14 @@
+const answers = [ 
+  {answer: "Central Perk", text: "Central Perk", "Bob's Burgers", "Tom's Restaurant", "Twede's Cafe"}
+  {answer: "1992", text: "1994", "1996", "1992", "1998"}
+
+
+]
+
+function getAnswers() {
+  return answers
+}
+
+export {
+  getAnswers
+}
---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*------------ Constants ------------*/
+import { getRandomPrompt } from "../data/prompts"
+import { getAnswers } from "../data/answers"
+
+
+
+/*------------ Variables ------------*/
+const prompts = []
+const answers = []
+
+
+
+
+/*---- Cached Element References ----*/
+const promptContainer = document.querySelector('#prompt-container')
+const answerContainer = document.querySelector('#answer-container')
+
+
+/*--------- Event Listeners ---------*/
+answerContainer.addEventListener('click')
+
+
+
+
+
+
+/*------------ Functions ------------*/
