6789bc595b17cfac36d093deb3932bad1e4c851c
jfranco <julian.franco@icloud.com>
2023-03-31 12:12:07 -0400

    edit array typos in prompts and answers

--- data/answers.js
+++ data/answers.js
@@ -1,6 +1,6 @@
 const answers = [ 
-  {answer: "Central Perk", text: "Central Perk", "Bob's Burgers", "Tom's Restaurant", "Twede's Cafe"}
-  {answer: "1992", text: "1994", "1996", "1992", "1998"}
+  {answer: "Central Perk", text: "Central Perk", text: "Bob's Burgers", text:"Tom's Restaurant", text: "Twede's Cafe"},
+  {answer: "1992", text: "1994", text: "1996", text: "1992", text: "1998"}
 
 
 ]
--- data/prompts.js
+++ data/prompts.js
@@ -1,6 +1,6 @@
 const prompts = [
   {prompt: true, text: "Just down the street from Monica's apartment, Central Perk was a fictional coffee shop and gathering spot for the title characters of what long-running sitcom?"},
-  {prompt: true, text: "The "Dream Team" won Olympic gold in what year's games?"}
+  {prompt: true, text: "The Dream Team won Olympic gold in what year's games?"}
 ]
 
 
